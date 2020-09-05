67c9ec8dbe5baa894576577786078d61e78055be
Jordi Riera <jordiriera@Jordis-MacBook-Pro.local>
2020-09-05 13:03:13 -0400

    add nginx for landing_page

---
+++ client/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "client",
+    "version": "12.0.0.1.0",
+    "license": "AGPL-3",
+    "depends": [
+        "crm",
+        "l10n_ca",
+        "website",
+        "website_blog",
+        "contacts",
+
+    ],
+    "category": "Custom",
+    "data": [
+    ],
+    "application": True,
+}
---
+++ landing_page/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "landing_page",
+    "version": "12.0.0.1.0",
+    "license": "AGPL-3",
+    "depends": [
+        "website",
+    ],
+    "category": "Custom",
+    "data": [
+    ],
+    "application": True,
+}
