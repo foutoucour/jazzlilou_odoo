3805c64866a2a6aacd1f662a1e1764a83425bfa3
Jordi Riera <kender.jr@gmail.com>
2020-09-13 15:41:22 -0400

    only one domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(edithcourtial.com www.edithcourtial.com)
+domains=(edithcourtial.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="tech@edithcourtial.com" # Adding a valid address is strongly recommended
